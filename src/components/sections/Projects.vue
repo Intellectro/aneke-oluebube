fae33baba2dcae1fe7c0b819f5557f382e9f7514
intellectro <oluebubeaneke247@gmail.com>
2025-03-22 16:30:49 -0700

    updated the project page

--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -1,20 +1,15 @@
 <script setup>
-    import { computed, onMounted, useTemplateRef } from 'vue';
+    import { computed } from 'vue';
     import { ProjectCard } from '..';
     import { useStore } from 'vuex';
 
     const store = useStore();
 
     const projects_data = computed(() => store.getters.projectsHandler);
-    const top = useTemplateRef("topr");
-    
-    onMounted(() => {
-        console.log(top.value.offsetTop);
-    });
 </script>
 
 <template>
-    <div ref="topr" class="flex flex-col gap-y-2 mt-10 md:mt-20 lg:mt-26">
+    <div class="flex flex-col gap-y-2 mt-10 md:mt-20 lg:mt-26">
         <ProjectCard v-for="(projects, index) in projects_data" :key="index" v-bind="projects" />
     </div>
 </template>
